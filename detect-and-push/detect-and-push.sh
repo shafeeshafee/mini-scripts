8541632834496199f1a9bc0dbf1805df3bed349b
shafeeshafee <shafeelinks@gmail.com>
2024-08-30 20:43:37 -0400

    add logic to check for creation of new files

--- detect-and-push/detect-and-push.sh
+++ detect-and-push/detect-and-push.sh
@@ -1,25 +1,27 @@
-# mydir
+#!/bin/bash
 
-
-# uses git's proprietory `ls-files` to see what files are being tracked
-# since ls-files only shows tracked files, --others shows untracked files
-# --exclude-standard exlcudes files that are git ignored
-# --error-unmatch exits with non-zero status if any files are listed
-if git ls-files --others --exclude-standard --error-unmatch > /dev/null 2>&1; then
-    echo "Untracked files detected"
-    ls
-else
-    echo "No untracked files"
-fi
-
-
-# user input for path
-read -p "Enter a path: " path
-echo "$path"
+# Get's path from user input
+path="$1"
 
 # check if path exists
 if [[ -d "$path" ]]; then
-    echo "Path exists"
+    # Navigate to that path
+    cd "$path" || { echo "Path $path doesn't exist."; exit 1; }
+
+    # Uses Git's `ls-files` command to list files in the repository.
+    # The --others option includes untracked files, which are not part of the repository.
+    # The --exclude-standard option excludes files ignored by Git's .gitignore rules.
+    # The --error-unmatch option is typically used to check for the existence of specific files,
+    # but in this context, we avoid it to correctly handle the detection of any untracked files.
+    untracked_files=$(git ls-files --others --exclude-standard)
+
+    if [[ -n "$untracked_files" ]]; then
+        echo "Untracked files detected:"
+        echo "$untracked_files"
+        
+    else
+        echo "Clear. No new files."
+    fi
 else
-    echo "Doesn't exist"
+    echo "Path doesn't exist."
 fi
